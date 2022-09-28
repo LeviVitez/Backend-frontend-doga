8ec8d6f4afb6c77914aff99ce21e273e9696f76d
Bóta László Levente <bmwyolo555@gmail.com>
2022-09-28 14:33:42 +0200

    darkmode

--- src/index.js
+++ src/index.js
@@ -1,38 +1,30 @@
 import './style.css';
-import '.style.js'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+let url;
+let kepszelesseg;
+let kepkeretvastagsag;
+let kepkeretszine;
+
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+function kepUrl() {
+   // var element = document.img;
+   // document.getElementById("kep").src = "hackanm.gif";
+    document.getElementById("kep").style.width = document.getElementById("szelesseg").value + 'px';
+    document.getElementById("kep").style.border = document.getElementById("keret").value + 'px solid';
+    document.getElementById("kep").style.borderColor = document.getElementById("szin").value;
+ }
 
 
 
-class kepkeret{
-    #url
-    #kepszelesseg
-    #kepkeretvastagsag
-    #kepkeretszine
-
-    constructor (url, kepszelesseg, kepkeretvastagsag, kepkeretszine){
-        this.#url=url;
-        this.#kepszelesseg=kepszelesseg;
-        this.#kepkeretvastagsag=kepkeretvastagsag;
-        this.#kepkeretszine=kepkeretszine;
-    }
-
-    get url() {
-        return this.#url;
-    }
-    get kepszelesseg () {
-        return this.#kepszelesseg;
-    }
-    get kepkeretvastagsag () {
-        return this.#kepkeretvastagsag;
-    }
-    get kepkeretszine () {
-        return this.#kepkeretszine;
-    }
-
-
-
-
-
-
-
-}
+ 
+document.getElementById("darkMode").addEventListener("click", darkMode);
+document.getElementById("darkMode").addEventListener("click", darkMode);
+document.getElementById("szelesseg").addEventListener("keyup", kepUrl);
+document.getElementById("keret").addEventListener("keyup", kepUrl);
+document.getElementById("szin").addEventListener("keyup", kepUrl);
+document.addEventListener('DOMContentLoaded', init)
