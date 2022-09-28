3730e85d3341e72c1be0b27adef6e874563b919c
Bóta László Levente <bmwyolo555@gmail.com>
2022-09-28 12:20:18 +0200

    haladas

--- src/index.js
+++ src/index.js
@@ -2,3 +2,37 @@ import './style.css';
 import '.style.js'
 
 
+
+class kepkeret{
+    #url
+    #kepszelesseg
+    #kepkeretvastagsag
+    #kepkeretszine
+
+    constructor (url, kepszelesseg, kepkeretvastagsag, kepkeretszine){
+        this.#url=url;
+        this.#kepszelesseg=kepszelesseg;
+        this.#kepkeretvastagsag=kepkeretvastagsag;
+        this.#kepkeretszine=kepkeretszine;
+    }
+
+    get url() {
+        return this.#url;
+    }
+    get kepszelesseg () {
+        return this.#kepszelesseg;
+    }
+    get kepkeretvastagsag () {
+        return this.#kepkeretvastagsag;
+    }
+    get kepkeretszine () {
+        return this.#kepkeretszine;
+    }
+
+
+
+
+
+
+
+}
